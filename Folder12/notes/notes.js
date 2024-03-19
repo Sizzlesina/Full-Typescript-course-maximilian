55e25aa233558e360b70a3af719f8da8ce769919
Sizzlesina <www.sins604@gmail.com>
2024-03-19 20:02:10 +0330

    Folder 12

---
+++ Folder12/notes/notes.js
@@ -0,0 +1,12 @@
+{/* prettier-ignore */}
+/*
+@ How to use 3rd party libraries in Typescript?:
+++ lodash: 
+npm i --save-dev lodash // ! For the package itself
+npm i --save-dev @types/lodash // ! For the types of the lodash so the typescript can recognise the library types
+++ class transformer:
+npm i class-transformer --save
+npm i reflect-metadata --save
+++ class validator:
+npm i class-validator --save
+*/
