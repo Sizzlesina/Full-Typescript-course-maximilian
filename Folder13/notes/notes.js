32bfe43b5eb2cc01abfa46d235ea2e3a6e118bc3
Sizzlesina <www.sins604@gmail.com>
2024-03-20 13:38:09 +0330

    Folder 13

---
+++ Folder13/notes/notes.js
@@ -0,0 +1,17 @@
+/** @format */
+
+{/* prettier-ignore */}
+/*
+
+! I cant finish this project properly because the Google geocoding API is need a payment method and i dont have any credit card
+
+@ Using axios to fetch data:
+++ npm i --save axios
+++ import axios from "axios";
+
+- If you need extra types we must install them
+++ We can use the ".get" method from axios to get the data
+
+* Instead we can check on openLayers documentation
+
+ */
--- Folder13/src/app.ts
+++ Folder13/src/app.ts
@@ -1,1 +1,12 @@
-// Code goes here!
+/** @format */
+
+const form = document.querySelector("form")!;
+const addressInput = document.getElementById("address")! as HTMLInputElement;
+
+function searchAddressHandler(event: Event) {
+  event.preventDefault();
+  const enteredAddress = addressInput.value;
+
+}
+
+form.addEventListener("submit", searchAddressHandler);
